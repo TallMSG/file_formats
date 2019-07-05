ffbab8903aa15bd57d8e4888fe42fa595a272fcf
Sergey Myshliakov <s.myshliakov@yandex.ru>
2019-07-05 17:40:56 +0300

    First commit

---
+++ formats__json.py
@@ -0,0 +1,48 @@
+import json
+from pprint import pprint
+
+with open('files/newsafr.json', encoding="utf-8") as datafile:
+  json_data = json.load(datafile)
+
+
+dict1 = {}
+for k, v in json_data.items():
+  dict1 = v
+
+dict2 = {}
+for k, v in dict1.items():
+  dict2 = dict1["channel"]
+
+list3 = []
+for k, v in dict2.items():
+  list3 = dict2["items"]
+
+newslist = []
+for i in list3:
+  newscontent = i["description"]
+  wordlist = newscontent.split()
+  newslist.append(wordlist)
+
+jointnewslist = []
+for item in newslist:
+  for subitem in item:
+    jointnewslist.append(subitem)
+
+longwordslist =[]
+for item in jointnewslist:
+  wordlength = len(item)
+  if wordlength > 6:
+    longwordslist.append(item)
+
+import collections
+
+word_counter = collections.Counter(longwordslist).most_common(10)
+
+sorted_dict = dict(word_counter)
+
+print(f'Топ 10 самых часто встречающихся в новостях слов длиннее 6 символов:')
+for k, v in sorted_dict.items():
+    print(f'Слово "{k}" встречается {v} раз')
+
+
+
---
+++ formats_xml.py
@@ -0,0 +1,39 @@
+import xml.etree.ElementTree as ET
+
+tree = ET.parse('files/newsafr.xml')
+root = tree.getroot()
+
+titles = []
+for child in root:
+  for subchild in child:
+    if subchild.tag == 'item':
+      for endchild in subchild:
+        if endchild.tag == 'description':
+          text = endchild.text
+          words = text.split()
+          titles.append(words)
+
+jointnewslist = []
+for item in titles:
+  for subitem in item:
+    jointnewslist.append(subitem)
+
+longwordslist =[]
+for item in jointnewslist:
+  wordlength = len(item)
+  if wordlength > 6:
+    longwordslist.append(item)
+
+import collections
+
+word_counter = collections.Counter(longwordslist).most_common(10)
+
+sorted_dict = dict(word_counter)
+
+print(f'Топ 10 самых часто встречающихся в новостях слов длиннее 6 символов:')
+for k, v in sorted_dict.items():
+    print(f'Слово "{k}" встречается {v} раз')
+
+
+
+
